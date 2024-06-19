a7ec7ee65eab99b53fb5b2d07f7bd0c1db20be67
grucia3 <grucia3@gmail.com>
2024-06-19 23:07:13 +0300

    Updated URL to use environment variables

--- src/components/TelegramLogin.js
+++ src/components/TelegramLogin.js
@@ -4,9 +4,9 @@ const TelegramLogin = () => {
     useEffect(() => {
         const script = document.createElement('script');
         script.src = "https://telegram.org/js/telegram-widget.js?7";
-        script.setAttribute('data-telegram-login', 'cryptobattle'); // Замініть 'cryptobattlebot' на фактичне ім'я вашого бота
+        script.setAttribute('data-telegram-login', process.env.REACT_APP_TELEGRAM_BOT_USERNAME); // Використання змінної середовища
         script.setAttribute('data-size', 'large');
-        script.setAttribute('data-auth-url', 'https://cryptobattle-2591e5064184.herokuapp.com/auth/telegram/callback'); // Замініть 'your-backend-url' на фактичну URL вашого бекенду
+        script.setAttribute('data-auth-url', process.env.REACT_APP_BACKEND_URL + '/auth/telegram/callback'); // Використання змінної середовища
         script.setAttribute('data-request-access', 'write');
         script.async = true;
 
