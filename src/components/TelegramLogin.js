3331e64b0f9768f717e11c30b392e2982f14501a
grucia3 <grucia3@gmail.com>
2024-06-20 01:05:02 +0300

    Add node_modules and .env to .gitignore 2

--- src/components/TelegramLogin.js
+++ src/components/TelegramLogin.js
@@ -5,9 +5,9 @@ const TelegramLogin = () => {
     useEffect(() => {
         const script = document.createElement('script');
         script.src = "https://telegram.org/js/telegram-widget.js?7";
-        script.setAttribute('data-telegram-login', 'CryptoRPGBattleBot'); // Вставте фактичне ім'я вашого бота
+        script.setAttribute('data-telegram-login', process.env.REACT_APP_TELEGRAM_BOT_USERNAME); // Використання змінної середовища
         script.setAttribute('data-size', 'large');
-        script.setAttribute('data-auth-url', `${process.env.REACT_APP_BACKEND_URL}/auth/telegram/callback`); // Використання змінної середовища
+        script.setAttribute('data-auth-url', `${process.env.REACT_APP_BACKEND_URL}/auth/telegram/callback`);
         script.setAttribute('data-request-access', 'write');
         script.async = true;
 
