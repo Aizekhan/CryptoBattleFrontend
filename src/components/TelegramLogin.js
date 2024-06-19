4aa2d75b078500d63f3c2185c1b1022d546ad258
grucia3 <grucia3@gmail.com>
2024-06-20 01:59:11 +0300

    Update Telegram login integration 6

--- src/components/TelegramLogin.js
+++ src/components/TelegramLogin.js
@@ -1,13 +1,12 @@
-// frontend/src/components/TelegramLogin.js
 import React, { useEffect } from 'react';
 
 const TelegramLogin = () => {
     useEffect(() => {
         const script = document.createElement('script');
         script.src = "https://telegram.org/js/telegram-widget.js?7";
-        script.setAttribute('data-telegram-login', process.env.REACT_APP_TELEGRAM_BOT_USERNAME); // Використання змінної середовища
+        script.setAttribute('data-telegram-login', process.env.REACT_APP_TELEGRAM_BOT_USERNAME);
         script.setAttribute('data-size', 'large');
-        script.setAttribute('data-auth-url', `${process.env.REACT_APP_BACKEND_URL}/auth/telegram/callback`);
+        script.setAttribute('data-auth-url', process.env.REACT_APP_BACKEND_URL + '/auth/telegram/callback');
         script.setAttribute('data-request-access', 'write');
         script.async = true;
 
