2a9160c8fe4cb4c9d4e22b0c92aa31d4d4a9c457
grucia3 <grucia3@gmail.com>
2024-06-20 22:20:19 +0300

    User name

--- src/App.js
+++ src/App.js
@@ -1,82 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './components/Home';
-import axios from 'axios';
-import jwt_decode from 'jwt-decode';
+import Mines from './components/Mines';
+import Hero from './components/Hero';
 import TopBar from './components/TopBar';
 import NavigationBar from './components/NavigationBar';
 import './App.css';
 
 function App() {
-    const [userInfo, setUserInfo] = useState({
-        username: '',
-        level: 0,
-        tapIncome: 0,
-        hourlyIncome: 0,
-        balance: 0,
-        heroes: [],
-        currentHero: null,
-        mines: []
-    });
+    const location = useLocation();
 
-    useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const token = urlParams.get('token');
-        if (token) {
-            const decoded = jwt_decode(token);
-            setUserInfo(prevState => ({
-                ...prevState,
-                username: decoded.username
-            }));
-
-            // Збереження токену в localStorage для подальшого використання
-            localStorage.setItem('authToken', token);
-
-            // Отримання додаткових даних користувача з бекенду
-            axios.get(`/api/users/${decoded.id}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }).then(response => {
-                setUserInfo(prevState => ({
-                    ...prevState,
-                    level: response.data.level,
-                    tapIncome: response.data.tapIncome,
-                    hourlyIncome: response.data.hourlyIncome,
-                    balance: response.data.balance,
-                    heroes: response.data.heroes,
-                    currentHero: response.data.currentHero,
-                    mines: response.data.mines
-                }));
-            }).catch(error => {
-                console.error('Error fetching user data:', error);
-            });
+    const getTitle = () => {
+        switch (location.pathname) {
+            case '/mines':
+                return 'Дохід в годину';
+            case '/hero':
+                return 'Магазин';
+            default:
+                return 'Дохід за тап';
         }
-    }, []);
+    };
 
     return (
+        <div className="app-container">
+            <TopBar title={getTitle()} />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/mines" element={<Mines />} />
+                <Route path="/hero" element={<Hero />} />
+            </Routes>
+            <NavigationBar />
+        </div>
+    );
+}
+
+function AppWrapper() {
+    return (
         <Router>
-            <div className="app-container">
-                <TopBar 
-                    username={userInfo.username}
-                    tapIncome={userInfo.tapIncome}
-                    level={userInfo.level}
-                    hourlyIncome={userInfo.hourlyIncome}
-                />
-                <nav className="nav-bar">
-                    <Link to="/">Home</Link>
-                    <Link to="/profile">Profile</Link>
-                    <Link to="/about">About</Link>
-                </nav>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/profile" element={<div>Profile Page</div>} />  {/* Замініть на ваш компонент профілю */}
-                    <Route path="/about" element={<div>About Page</div>} />  {/* Замініть на ваш компонент про додаток */}
-                </Routes>
-                <NavigationBar />
-            </div>
+            <App />
         </Router>
     );
 }
 
-export default App;
+export default AppWrapper;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import './TopBar.css';
 import userIcon from '../assets/images/user-icon.png';
+import levelIcon from '../assets/images/level.png';
 import friendsIcon from '../assets/images/friends.png';
 import binanceIcon from '../assets/images/binance-icon.png';
 import walletIcon from '../assets/images/wallet-icon.png';
-import levelIcon from '../assets/images/level.png'; // Іконка рівня
 
-const TopBar = ({ username, tapIncome, level, hourlyIncome }) => {
+const TopBar = ({ title }) => {
     return (
         <div className="top-bar">
             <div className="top-row">
                 <div className="user-info">
                     <img src={userIcon} alt="User" className="user-icon" />
-                    <span className="username">{username}</span>
-                    <div className="level-info">
-                        <img src={levelIcon} alt="Level" className="level-icon" />
-                        <span className="level">{level}</span>
-                    </div>
+                    <span className="username">Максим</span>
+                    <img src={levelIcon} alt="Level" className="level-icon" />
+                    <span className="level">0</span>
                 </div>
                 <div className="icon-bar">
                     <img src={binanceIcon} alt="Binance" className="binance-icon" />
@@ -25,15 +22,10 @@ const TopBar = ({ username, tapIncome, level, hourlyIncome }) => {
             </div>
             <div className="stats-bar">
                 <div className="stat">
-                    <span>Дохід за тап</span>
-                    <span className="value">{tapIncome}</span>
+                    <span>{title}</span>
                 </div>
                 <div className="stat">
-                    <img src={friendsIcon} alt="Friends" className="friends-icon" /> {/* Замість рівня */}
-                </div>
-                <div className="stat">
-                    <span>Дохід в годину</span>
-                    <span className="value">{hourlyIncome}</span>
+                    <img src={friendsIcon} alt="Додати друга" className="friends-icon" />
                 </div>
             </div>
         </div>
