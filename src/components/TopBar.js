7e7ebed5545904b2efe433006404c2ef565bc165
Aizekhan <hrytsenkomaksym@gmail.com>
2024-06-20 14:41:42 +0300

    Updated project with new changes

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -7,13 +7,15 @@ import walletIcon from '../assets/images/wallet-icon.png'; // Оновлений
 const TopBar = ({ username, tapIncome, level, hourlyIncome }) => {
     return (
         <div className="top-bar">
-            <div className="user-info">
-                <img src={userIcon} alt="User" className="user-icon" />
-                <span>{username}</span>
-            </div>
-            <div className="icon-bar">
-                <img src={binanceIcon} alt="Binance" className="binance-icon" />
-                <img src={walletIcon} alt="Wallet" className="wallet-icon" />
+            <div className="top-row">
+                <div className="user-info">
+                    <img src={userIcon} alt="User" className="user-icon" />
+                    <span className="username">{username}</span>
+                </div>
+                <div className="icon-bar">
+                    <img src={binanceIcon} alt="Binance" className="binance-icon" />
+                    <img src={walletIcon} alt="Wallet" className="wallet-icon" />
+                </div>
             </div>
             <div className="stats-bar">
                 <div className="stat">
