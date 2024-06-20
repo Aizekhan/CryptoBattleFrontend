92ef2fffe9b45ad5e64dcaa8fb3ca908a3e3fc78
grucia3 <grucia3@gmail.com>
2024-06-21 00:48:43 +0300

    User name

--- src/components/SecondaryBar.js
+++ src/components/SecondaryBar.js
@@ -1,6 +1,4 @@
-// src/components/SecondaryBar.js
-
-import React from 'react';
+/import React from 'react';
 import { Link } from 'react-router-dom';
 import homeIcon from '../assets/images/home-icon.png';
 import friendsIcon from '../assets/images/friends.png';
@@ -11,9 +9,11 @@ const SecondaryBar = () => {
         <div className="secondary-bar">
             <Link to="/home" className="nav-button">
                 <img src={homeIcon} alt="Home" className="nav-icon" />
+                <span className="nav-text">Home</span>
             </Link>
             <Link to="/friends" className="nav-button">
                 <img src={friendsIcon} alt="Friends" className="nav-icon" />
+                <span className="nav-text">Friends</span>
             </Link>
         </div>
     );
