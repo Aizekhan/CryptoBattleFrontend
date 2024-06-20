6d552c08ed189c5824a8ebb0e39b0cad5bfb0299
Aizekhan <hrytsenkomaksym@gmail.com>
2024-06-20 14:34:22 +0300

    Updated project with new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import CombinedBar from './components/CombinedBar';
+import TopBar from './components/TopBar';
 import NavigationBar from './components/NavigationBar';
 
 function App() {
-    const [userInfo] = useState({
-        username: 'Максим',
+    const [userInfo, setUserInfo] = useState({
+        username: '',
         level: 7,
         tapIncome: '+15',
         hourlyIncome: '+992,27K',
         balance: '24 160 678'
     });
 
+    useEffect(() => {
+        const storedUserInfo = JSON.parse(localStorage.getItem('userInfo'));
+        if (storedUserInfo) {
+            setUserInfo((prevInfo) => ({
+                ...prevInfo,
+                username: storedUserInfo.username
+            }));
+        }
+    }, []);
+
     return (
         <Router>
             <div className="app-container">
-                <CombinedBar 
+                <TopBar 
                     username={userInfo.username}
                     tapIncome={userInfo.tapIncome}
                     level={userInfo.level}
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -5,12 +5,12 @@ import friendsIcon from '../assets/images/friends.png';
 import binanceIcon from '../assets/images/binance-icon.png';
 import walletIcon from '../assets/images/wallet-icon.png'; // Оновлений шлях до іконки
 
-const TopBar = ({ username }) => {
+const TopBar = ({ username, tapIncome, level, hourlyIncome }) => {
     return (
         <div className="top-bar">
             <div className="user-info">
                 <img src={userIcon} alt="User" className="user-icon" />
-                <span>{username} (Trainer)</span>
+                <span>{username}</span>
             </div>
             <div className="friends-info">
                 <img src={friendsIcon} alt="Friends" className="friends-icon" />
@@ -18,6 +18,20 @@ const TopBar = ({ username }) => {
             <div className="binance-info">
                 <img src={binanceIcon} alt="Binance" className="binance-icon" />
                 <img src={walletIcon} alt="Wallet" className="wallet-icon" />
+            </div>
+            <div className="stats-bar">
+                <div className="stat">
+                    <span>Дохід за тап</span>
+                    <span className="value">{tapIncome}</span>
+                </div>
+                <div className="stat">
+                    <span>Рівень акаунту</span>
+                    <span className="value">{level}</span>
+                </div>
+                <div className="stat">
+                    <span>Дохід в годину</span>
+                    <span className="value">{hourlyIncome}</span>
+                </div>
             </div>
         </div>
     );
