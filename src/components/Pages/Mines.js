8ee4e1563e219be30763fa6536acfd13e948c011
grucia3 <grucia3@gmail.com>
2024-06-21 01:23:18 +0300

    User name

--- src/components/Pages/Mines.js
+++ src/components/Pages/Mines.js
@@ -2,17 +2,26 @@
 
 import React from 'react';
 import './Mines.css';
+import img1 from '../assets/images/MinesPageImages/1.png';
+import img2 from '../assets/images/MinesPageImages/2.png';
+import img3 from '../assets/images/MinesPageImages/3.png';
+import img4 from '../assets/images/MinesPageImages/4.png';
+import img5 from '../assets/images/MinesPageImages/5.png';
+import img6 from '../assets/images/MinesPageImages/6.png';
+import img7 from '../assets/images/MinesPageImages/7.png';
+import img8 from '../assets/images/MinesPageImages/8.png';
+import img9 from '../assets/images/MinesPageImages/9.png';
 
 const minesData = [
-    { id: 1, cost: 100, income: 10 },
-    { id: 2, cost: 200, income: 20 },
-    { id: 3, cost: 300, income: 30 },
-    { id: 4, cost: 400, income: 40 },
-    { id: 5, cost: 500, income: 50 },
-    { id: 6, cost: 600, income: 60 },
-    { id: 7, cost: 700, income: 70 },
-    { id: 8, cost: 800, income: 80 },
-    { id: 9, cost: 900, income: 90 },
+    { id: 1, cost: 100, income: 10, image: img1 },
+    { id: 2, cost: 200, income: 20, image: img2 },
+    { id: 3, cost: 300, income: 30, image: img3 },
+    { id: 4, cost: 400, income: 40, image: img4 },
+    { id: 5, cost: 500, income: 50, image: img5 },
+    { id: 6, cost: 600, income: 60, image: img6 },
+    { id: 7, cost: 700, income: 70, image: img7 },
+    { id: 8, cost: 800, income: 80, image: img8 },
+    { id: 9, cost: 900, income: 90, image: img9 },
 ];
 
 const Mines = () => {
@@ -22,7 +31,7 @@ const Mines = () => {
             <div className="mines-grid">
                 {minesData.map(mine => (
                     <div key={mine.id} className="mine-item">
-                        <img src={`/assets/MinesPageImages/${mine.id}.png`} alt={`Mine ${mine.id}`} />
+                        <img src={mine.image} alt={`Mine ${mine.id}`} />
                         <div className="mine-cost">Cost: {mine.cost} gold</div>
                         <div className="mine-income">+{mine.income} gold/hour</div>
                     </div>
