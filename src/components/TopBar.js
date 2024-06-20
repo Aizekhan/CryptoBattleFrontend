d51cb7a9bf0826416151781369fd08bf7a9debd4
grucia3 <grucia3@gmail.com>
2024-06-20 20:58:08 +0300

    User name

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import './TopBar.css';
 import userIcon from '../assets/images/user-icon.png';
+import friendsIcon from '../assets/images/friends.png';
 import binanceIcon from '../assets/images/binance-icon.png';
 import walletIcon from '../assets/images/wallet-icon.png';
-import friendsIcon from '../assets/images/friends.png'; // Іконка для Friends
+import levelIcon from '../assets/images/level.png'; // Іконка рівня
 
-const TopBar = ({ username, level, tapIncome, hourlyIncome }) => {
+const TopBar = ({ username, tapIncome, level, hourlyIncome }) => {
     return (
         <div className="top-bar">
             <div className="top-row">
                 <div className="user-info">
                     <img src={userIcon} alt="User" className="user-icon" />
                     <span className="username">{username}</span>
-                    <span className="level">Рівень: {level}</span> {/* Додавання рівня користувача */}
+                    <div className="level-info">
+                        <img src={levelIcon} alt="Level" className="level-icon" />
+                        <span className="level">{level}</span>
+                    </div>
                 </div>
                 <div className="icon-bar">
-                    <img src={friendsIcon} alt="Friends" className="friends-icon" /> {/* Додавання кнопки Friends */}
                     <img src={binanceIcon} alt="Binance" className="binance-icon" />
                     <img src={walletIcon} alt="Wallet" className="wallet-icon" />
                 </div>
@@ -24,6 +27,9 @@ const TopBar = ({ username, level, tapIncome, hourlyIncome }) => {
                 <div className="stat">
                     <span>Дохід за тап</span>
                     <span className="value">{tapIncome}</span>
+                </div>
+                <div className="stat">
+                    <img src={friendsIcon} alt="Friends" className="friends-icon" /> {/* Замість рівня */}
                 </div>
                 <div className="stat">
                     <span>Дохід в годину</span>
