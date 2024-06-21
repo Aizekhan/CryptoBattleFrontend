d7af9f5495f159b61cbaacb67e20f7c78a184c7c
grucia3 <grucia3@gmail.com>
2024-06-21 23:42:55 +0300

    Panels and Mines

--- src/components/Panels/SecondaryBar.js
+++ src/components/Panels/SecondaryBar.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import homeIcon from '../../assets/images/home-icon.png';
-import friendsIcon from '../../assets/images/friends-icon.png';
 import pickaxeIcon from '../../assets/images/pickaxe-icon.png';
 import { useUserStats } from '../UserStatsContext';
+import './MainLayout.css'; // Оновлення стилів, щоб використовувати MainLayout.css
 
 const SecondaryBar = () => {
     const { userStats } = useUserStats();
 
     return (
         <div className="secondary-bar">
-            <Link to="/home" className="nav-button">
-                <img src={homeIcon} alt="Home" className="home-icon" />
-                <div>Home</div>
-            </Link>
             <div className="income-info">
                 <img src={pickaxeIcon} alt="Income" className="income-icon" />
-                <span>{userStats.hourlyIncome} gold/hour</span>
+                <span>{userStats.hourlyIncome}</span>
             </div>
-            <Link to="/friends" className="nav-button">
-                <img src={friendsIcon} alt="Friends" className="friends-icon" />
-                <div>Friends</div>
-            </Link>
         </div>
     );
 };
--- src/components/Panels/TopBar.js
+++ src/components/Panels/TopBar.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import userIcon from '../../assets/images/user-icon.png';
 import walletIcon from '../../assets/images/wallet-icon.png';
 import coinImage from '../../assets/images/coin.png';
 import { useUserStats } from '../UserStatsContext';
+import './MainLayout.css'; // Оновлення стилів, щоб використовувати MainLayout.css
 
 const TopBar = () => {
     const { userStats } = useUserStats();
+    const [balance, setBalance] = useState(userStats.balance);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setBalance(prevBalance => prevBalance + userStats.hourlyIncome / 3600);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [userStats.hourlyIncome]);
 
     return (
         <div className="top-bar">
@@ -15,7 +25,7 @@ const TopBar = () => {
             </div>
             <div className="balance-info">
                 <img src={coinImage} alt="Coin" className="coin-icon" />
-                <span>{Math.floor(userStats.balance)} gold</span>
+                <span>{Math.floor(balance)}</span>
             </div>
             <div className="wallet-info">
                 <img src={walletIcon} alt="Wallet" />
