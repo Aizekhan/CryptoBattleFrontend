d21c4525878de75c073d6b6a7d9970f96abe398f
Aizekhan <hrytsenkomaksym@gmail.com>
2024-06-22 11:45:45 +0300

    Mines

--- src/components/Pages/Mines.js
+++ src/components/Pages/Mines.js
@@ -14,8 +14,9 @@ const Mines = () => {
     };
 
     const handleUpgrade = () => {
-        if (userStats.balance >= selectedMine.upgradeCost) {
-            spendBalance(selectedMine.upgradeCost);
+        const upgradeCost = selectedMine.cost * 2 ** (selectedMine.currentLevel - 1);
+        if (userStats.balance >= upgradeCost) {
+            spendBalance(upgradeCost);
             upgradeMine(selectedMine.id);
             setUpgradePanelVisible(false);
         } else {
@@ -30,27 +31,29 @@ const Mines = () => {
                 {userStats.mines.map((mine) => (
                     <div
                         key={mine.id}
-                        className="mine-item"
+                        className={`mine-item ${mine.locked ? 'locked' : ''}`}
                         onClick={() => !mine.locked && handleMineClick(mine)}
                     >
                         <img src={mine.locked ? lockImage : mine.img} alt={`Mine ${mine.id}`} />
-                        <div className="mine-cost">Вартість: {mine.upgradeCost} золота</div>
+                        <div className="mine-cost">Вартість: {mine.cost} золота</div>
                         <div className="mine-income">+{mine.income} золота/год</div>
                         <div className="mine-level">lvl: {mine.currentLevel}</div>
                     </div>
                 ))}
             </div>
             {upgradePanelVisible && (
-                <div className="mine-upgrade-panel">
-                    <div className="mine-info">
-                        <img src={selectedMine.img} alt={`Mine ${selectedMine.id}`} />
-                        <div>Вартість апгрейду: {selectedMine.upgradeCost} золота</div>
-                        <div>Дохід: +{selectedMine.income} золота/год</div>
-                        <div>Поточний рівень: {selectedMine.currentLevel}</div>
-                    </div>
-                    <div className="upgrade-buttons">
-                        <button onClick={handleUpgrade}>Upgrade</button>
-                        <button onClick={() => setUpgradePanelVisible(false)}>Close</button>
+                <div className="overlay">
+                    <div className="mine-upgrade-panel">
+                        <div className="mine-info">
+                            <img src={selectedMine.img} alt={`Mine ${selectedMine.id}`} />
+                            <div>Вартість апгрейду: {selectedMine.cost * 2 ** (selectedMine.currentLevel - 1)} золота</div>
+                            <div>Дохід: +{selectedMine.income} золота/год</div>
+                            <div>Поточний рівень: {selectedMine.currentLevel}</div>
+                        </div>
+                        <div className="upgrade-buttons">
+                            <button onClick={handleUpgrade}>Upgrade</button>
+                            <button onClick={() => setUpgradePanelVisible(false)}>Close</button>
+                        </div>
                     </div>
                 </div>
             )}
