ae6ab7dab361caf5a4161adfd1174a6004bdae20
Aizekhan <hrytsenkomaksym@gmail.com>
2024-06-22 12:02:01 +0300

    Mines

--- src/components/Pages/Mines.js
+++ src/components/Pages/Mines.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import { useUserStats } from '../UserStatsContext';
+import { minesData } from '../../data/minesData';
 import './Mines.css';
-import { useUserStats } from '../UserStatsContext'; // Оновлений шлях
-import { minesData, lockImage } from '../../data/minesData';
 import UpgradePanel from '../Panels/UpgradePanel';
 
 const Mines = () => {
@@ -13,13 +13,17 @@ const Mines = () => {
         setSelectedMine(null);
     };
 
+    const handleClose = () => {
+        setSelectedMine(null);
+    };
+
     return (
         <div className="mines-container">
             <h1>Mines Page</h1>
             <div className="mines-grid">
-                {userStats.mines.map((mine) => (
+                {userStats.mines.map(mine => (
                     <div key={mine.id} className="mine-item" onClick={() => setSelectedMine(mine)}>
-                        <img src={mine.locked ? lockImage : mine.img} alt={`Mine ${mine.id}`} />
+                        <img src={mine.img} alt={`Mine ${mine.id}`} />
                         <div className="mine-cost">Вартість: {mine.cost} золота</div>
                         <div className="mine-income">+{mine.income} золота/год</div>
                         <div className="mine-level">lvl: {mine.currentLevel}</div>
@@ -27,13 +31,14 @@ const Mines = () => {
                 ))}
             </div>
             {selectedMine && (
-                <UpgradePanel
-                    mine={selectedMine}
-                    onClose={() => setSelectedMine(null)}
-                    onUpgrade={handleUpgrade}
+                <UpgradePanel 
+                    mine={selectedMine} 
+                    onClose={handleClose} 
+                    onUpgrade={handleUpgrade} 
                 />
             )}
         </div>
     );
 };
 
+export default Mines;
