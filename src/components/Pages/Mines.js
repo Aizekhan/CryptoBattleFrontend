7de8d24721b89fad8db6c0776f493441f0c95b88
Aizekhan <hrytsenkomaksym@gmail.com>
2024-06-22 12:53:29 +0300

    Mines

--- src/components/Pages/Mines.js
+++ src/components/Pages/Mines.js
@@ -1,48 +1,41 @@
-// src/components/Pages/Mines.js
-
 import React, { useState } from 'react';
-import { minesData } from '../../data/minesData';
+import './Mines.css';
 import { useUserStats } from '../UserStatsContext';
 import UpgradePanel from '../Panels/UpgradePanel';
-import './Mines.css';
 
 const Mines = () => {
-    const { userStats, upgradeMine, spendBalance } = useUserStats();
+    const { userStats, upgradeMine } = useUserStats();
     const [selectedMine, setSelectedMine] = useState(null);
 
-    const handleUpgradeClick = (mine) => {
-        setSelectedMine(mine);
-    };
-
-    const closeUpgradePanel = () => {
+    const handleUpgrade = (mineId) => {
+        upgradeMine(mineId);
         setSelectedMine(null);
     };
 
-    const upgradeSelectedMine = () => {
-        if (selectedMine) {
-            upgradeMine(selectedMine.id);
-            closeUpgradePanel();
-        }
-    };
-
     return (
-        <div className="mines-container">
+        <div className="mines-page">
             <h1>Mines Page</h1>
             <div className="mines-grid">
-                {userStats.mines.map((mine, index) => (
-                    <div key={mine.id} className="mine-item" onClick={() => handleUpgradeClick(mine)}>
-                        <img src={mine.img} alt={`Mine ${mine.id}`} />
-                        <div className="mine-cost">Вартість: {mine.cost} золота</div>
-                        <div className="mine-income">+{mine.income} золота/год</div>
-                        <div>lvl: {mine.level}</div>
+                {userStats.mines.map(mine => (
+                    <div key={mine.id} className="mine-item">
+                        <img
+                            src={mine.locked ? '/assets/images/lock.png' : mine.img}
+                            alt={`Mine ${mine.id}`}
+                            onClick={() => !mine.locked && setSelectedMine(mine)}
+                        />
+                        <div className="mine-info">
+                            <p>Вартість: {mine.cost} золота</p>
+                            <p>+{mine.income} золота/год</p>
+                            <p>lvl: {mine.currentLevel}</p>
+                        </div>
                     </div>
                 ))}
             </div>
             {selectedMine && (
                 <UpgradePanel
                     mine={selectedMine}
-                    onClose={closeUpgradePanel}
-                    onUpgrade={upgradeSelectedMine}
+                    onClose={() => setSelectedMine(null)}
+                    onUpgrade={handleUpgrade}
                 />
             )}
         </div>
