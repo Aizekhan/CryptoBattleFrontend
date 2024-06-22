e0112b3b022c92b83953f0fa89d29ec82c888e7e
Aizekhan <hrytsenkomaksym@gmail.com>
2024-06-22 11:58:18 +0300

    Mines

--- src/components/Panels/UpgradePanel.js
+++ src/components/Panels/UpgradePanel.js
@@ -1,20 +1,22 @@
-// src/components/Panels/UpgradePanel.js
-
 import React from 'react';
 import './UpgradePanel.css';
+import { useUserStats } from '../UserStatsContext';
 
-const UpgradePanel = ({ mine, onUpgrade, onClose }) => {
+const UpgradePanel = ({ mine, onClose, onUpgrade }) => {
+    const { userStats } = useUserStats();
+    const upgradeCost = mine.cost * 2 ** (mine.currentLevel - 1);
+
     return (
         <div className="upgrade-panel">
             <h2>Upgrade Mine</h2>
-            <div className="mine-info">
-                <img src={mine.img} alt={`Mine ${mine.id}`} />
-                <div>Вартість апгрейду: {mine.upgradeCost} золота</div>
-                <div>Дохід: +{mine.income} золота/год</div>
-                <div>Поточний рівень: {mine.level}</div>
+            <img src={mine.img} alt={`Mine ${mine.id}`} />
+            <div className="upgrade-cost">Вартість апгрейду: {upgradeCost} золота</div>
+            <div className="current-income">Дохід: +{mine.income} золота/год</div>
+            <div className="current-level">Поточний рівень: {mine.currentLevel}</div>
+            <div className="button-container">
+                <button className="upgrade-button" onClick={() => onUpgrade(mine.id)}>Upgrade</button>
+                <button className="close-button" onClick={onClose}>Close</button>
             </div>
-            <button onClick={onUpgrade}>Upgrade</button>
-            <button onClick={onClose}>Close</button>
         </div>
     );
 };
