a049aaa2b124c00cd7424d26522cb127e1d632ce
grucia3 <grucia3@gmail.com>
2024-06-27 21:59:25 +0300

    mines

--- src/components/Pages/Mines/MinesGold.js
+++ src/components/Pages/Mines/MinesGold.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CardList from '../Cards/CardList';
+import CardList from '../../Cards/CardList';
 import image1 from '../../assets/images/MinesPageImages/1.png';
 import image2 from '../../assets/images/MinesPageImages/2.png';
 import image3 from '../../assets/images/MinesPageImages/3.png';
