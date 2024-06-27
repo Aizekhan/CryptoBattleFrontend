c9595e29669824957290754708ee2a36d87cd0b4
grucia3 <grucia3@gmail.com>
2024-06-27 11:47:30 +0300

    MinesPages

--- src/components/Pages/Battle/Battle.js
+++ src/components/Pages/Battle/Battle.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import SubNavigation from '../Panels/SubNavigation';
+import SubNavigation from '../../Panels/SubNavigation';
 
 const Battle = () => {
     const subPages = [
