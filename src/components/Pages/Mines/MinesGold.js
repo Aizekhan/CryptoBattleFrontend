2c54fb367f9f37376d5d7d6789fc55fff42d41a1
grucia3 <grucia3@gmail.com>
2024-06-27 22:20:22 +0300

    mines

--- src/components/Pages/Mines/MinesGold.js
+++ src/components/Pages/Mines/MinesGold.js
@@ -12,11 +12,11 @@ import image9 from '../../../assets/images/MinesPageImages/9.png';
 
 const MinesGold = () => {
     const minesCards = [
-        { id: 1, name: 'Gold Mine 1', level: 0, effect: 'Produces gold', img: image1, prerequisites: [], upgradeCost: 100 },
-        { id: 2, name: 'Gold Mine 2', level: 0, effect: 'Produces more gold', img: image2, prerequisites: [{ id: 1, level: 3 }], upgradeCost: 200 },
-        { id: 3, name: 'Gold Mine 3', level: 0, effect: 'Produces even more gold', img: image3, prerequisites: [{ id: 2, level: 3 }], upgradeCost: 300 },
-        { id: 4, name: 'Resource Mine 1', level: 0, effect: 'Produces resources', img: image4, prerequisites: [], upgradeCost: 150 },
-        { id: 5, name: 'Resource Mine 2', level: 0, effect: 'Produces more resources', img: image5, prerequisites: [{ id: 4, level: 3 }], upgradeCost: 250 },
+        { id: 1, name: 'Gold Mine 1', level: 0, effect: 'Produces gold', img: image1, prerequisites: [], upgradeCost: 0 },
+        { id: 2, name: 'Gold Mine 2', level: 0, effect: 'Produces more gold', img: image2, prerequisites: [{ id: 1, level: 3 }], upgradeCost: 0 },
+        { id: 3, name: 'Gold Mine 3', level: 0, effect: 'Produces even more gold', img: image3, prerequisites: [{ id: 2, level: 3 }], upgradeCost: 0 },
+        { id: 4, name: 'Resource Mine 1', level: 0, effect: 'Produces resources', img: image4, prerequisites: [], upgradeCost: 0 },
+        { id: 5, name: 'Resource Mine 2', level: 0, effect: 'Produces more resources', img: image5, prerequisites: [{ id: 4, level: 3 }], upgradeCost: 0 },
         { id: 6, name: 'Resource Mine 3', level: 0, effect: 'Produces even more resources', img: image6, prerequisites: [{ id: 5, level: 3 }], upgradeCost: 350 },
         { id: 7, name: 'Crystal Mine 1', level: 0, effect: 'Produces crystals', img: image7, prerequisites: [], upgradeCost: 400 },
         { id: 8, name: 'Crystal Mine 2', level: 0, effect: 'Produces more crystals', img: image8, prerequisites: [{ id: 7, level: 3 }], upgradeCost: 450 },
