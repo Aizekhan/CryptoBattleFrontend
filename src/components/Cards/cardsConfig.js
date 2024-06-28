f7d0a9ab15a33da0584059d0174344d7519773f1
grucia3 <grucia3@gmail.com>
2024-06-28 21:33:38 +0300

    subPages

--- src/components/Cards/cardsConfig.js
+++ src/components/Cards/cardsConfig.js
@@ -87,16 +87,17 @@ export const heroCards = [
 ];
 
 export const minesCards = [
-    { id: 1, name: 'Gold Mine 1', level: 0, effect: 'Produces gold', img: mine1, prerequisites: [], upgradeCost: 100, tag: 'mine' },
-    { id: 2, name: 'Gold Mine 2', level: 0, effect: 'Produces more gold', img: mine2, prerequisites: [{ id: 1, level: 3 }], upgradeCost: 200, tag: 'mine' },
-    { id: 3, name: 'Gold Mine 3', level: 0, effect: 'Produces even more gold', img: mine3, prerequisites: [{ id: 2, level: 3 }], upgradeCost: 300, tag: 'mine' },
-    { id: 4, name: 'Resource Mine 1', level: 0, effect: 'Produces resources', img: mine4, prerequisites: [], upgradeCost: 150, tag: 'mine' },
-    { id: 5, name: 'Resource Mine 2', level: 0, effect: 'Produces more resources', img: mine5, prerequisites: [{ id: 4, level: 3 }], upgradeCost: 250, tag: 'mine' },
-    { id: 6, name: 'Resource Mine 3', level: 0, effect: 'Produces even more resources', img: mine6, prerequisites: [{ id: 5, level: 3 }], upgradeCost: 350, tag: 'mine' },
-    { id: 7, name: 'Crystal Mine 1', level: 0, effect: 'Produces crystals', img: mine7, prerequisites: [], upgradeCost: 400, tag: 'mine' },
-    { id: 8, name: 'Crystal Mine 2', level: 0, effect: 'Produces more crystals', img: mine8, prerequisites: [{ id: 7, level: 3 }], upgradeCost: 450, tag: 'mine' },
-    { id: 9, name: 'Crystal Mine 3', level: 0, effect: 'Produces even more crystals', img: mine9, prerequisites: [{ id: 8, level: 3 }, { id: 7, level: 5 }], upgradeCost: 500, tag: 'mine' },
+    { id: 1, name: 'Gold Mine 1', effect: 'Produces gold', level: 0, img: mine1, prerequisites: [], upgradeCost: 100, tag: 'goldMine', scaleUpgrade: 1.1, scaleIncome: 1.2, baseIncome: 10 },
+    { id: 2, name: 'Gold Mine 2', effect: 'Produces more gold', level: 0, img: mine2, prerequisites: [{ id: 1, level: 3 }], upgradeCost: 200, tag: 'goldMine', scaleUpgrade: 1.2, scaleIncome: 1.3, baseIncome: 20 },
+    { id: 3, name: 'Gold Mine 3', effect: 'Produces even more gold', level: 0, img: mine3, prerequisites: [{ id: 2, level: 3 }], upgradeCost: 300, tag: 'goldMine', scaleUpgrade: 1.3, scaleIncome: 1.4, baseIncome: 30 },
+    { id: 4, name: 'Resource Mine 1', effect: 'Produces resources', level: 0, img: mine4, prerequisites: [], upgradeCost: 150, tag: 'resourceMine', scaleUpgrade: 1.1, scaleIncome: 1.2, baseIncome: 15 },
+    { id: 5, name: 'Resource Mine 2', effect: 'Produces more resources', level: 0, img: mine5, prerequisites: [{ id: 4, level: 3 }], upgradeCost: 250, tag: 'resourceMine', scaleUpgrade: 1.2, scaleIncome: 1.3, baseIncome: 30 },
+    { id: 6, name: 'Resource Mine 3', effect: 'Produces even more resources', level: 0, img: mine6, prerequisites: [{ id: 5, level: 3 }], upgradeCost: 350, tag: 'resourceMine', scaleUpgrade: 1.3, scaleIncome: 1.4, baseIncome: 45 },
+    { id: 7, name: 'Crystal Mine 1', effect: 'Produces crystals', level: 0, img: mine7, prerequisites: [], upgradeCost: 400, tag: 'crystalMine', scaleUpgrade: 1.1, scaleIncome: 1.2, baseIncome: 5 },
+    { id: 8, name: 'Crystal Mine 2', effect: 'Produces more crystals', level: 0, img: mine8, prerequisites: [{ id: 7, level: 3 }], upgradeCost: 450, tag: 'crystalMine', scaleUpgrade: 1.2, scaleIncome: 1.3, baseIncome: 10 },
+    { id: 9, name: 'Crystal Mine 3', effect: 'Produces even more crystals', level: 0, img: mine9, prerequisites: [{ id: 8, level: 3 }, { id: 7, level: 5 }], upgradeCost: 500, tag: 'crystalMine', scaleUpgrade: 1.3, scaleIncome: 1.4, baseIncome: 15 },
 ];
+
 
 export const marketCards = [
     { id: 1, name: 'Hero Orc', effect: 'A strong orc hero', level: 1, upgradeCost: 100, img: marketHeroOrc, tag: 'market', prerequisites: [] },
