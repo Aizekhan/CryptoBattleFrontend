c4d81d1ce356474847073cf0f32ede214ec896df
grucia3 <grucia3@gmail.com>
2024-06-28 16:22:12 +0300

    subPages

---
+++ src/components/Pages/Quests/Active.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Active = () => {
+    return (
+        <div>
+            <h1>Active Page</h1>
+            <p>Welcome to the Active page. Manage your active quests here.</p>
+        </div>
+    );
+};
+
+export default Active;
