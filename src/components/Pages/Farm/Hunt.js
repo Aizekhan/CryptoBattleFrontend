d43ef11c4f1208169578a8551702c994e5af2c63
grucia3 <grucia3@gmail.com>
2024-06-28 15:03:17 +0300

    subPages

---
+++ src/components/Pages/Farm/Hunt.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hunt = () => {
+    return (
+        <div>
+            <h1>Hunt Page</h1>
+            <p>Welcome to the Hunt page. This is where you can manage your hunting activities.</p>
+        </div>
+    );
+};
+
+export default Hunt;
---
+++ src/components/Pages/Farm/Locations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Locations = () => {
+    return (
+        <div>
+            <h1>Locations Page</h1>
+            <p>Welcome to the Locations page. This is where you can manage your locations.</p>
+        </div>
+    );
+};
+
+export default Locations;
