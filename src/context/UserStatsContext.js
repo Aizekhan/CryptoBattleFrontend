512fec99d0cff7f32a185e3f7c9793bd71df4d16
grucia3 <grucia3@gmail.com>
2024-06-30 04:17:09 +0300

    HeroSystem

--- src/context/UserStatsContext.js
+++ src/context/UserStatsContext.js
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import axios from 'axios';
 import heroesConfig from './heroesConfig'; // Імпорт конфігурації героїв
 
 const UserStatsContext = createContext();
@@ -8,7 +7,7 @@ export const useUserStats = () => useContext(UserStatsContext);
 
 export const UserStatsProvider = ({ children }) => {
     const [userStats, setUserStats] = useState({
-        username: '',
+        username: 'Player',
         level: 0,
         tapIncome: 1,
         hourlyIncome: 0,
@@ -33,22 +32,6 @@ export const UserStatsProvider = ({ children }) => {
             { id: 9, level: 0, baseIncome: 90 },
         ],
     });
-
-    useEffect(() => {
-        const fetchUserStats = async () => {
-            try {
-                const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/user-stats`);
-                if (response.data) {
-                    setUserStats(response.data);
-                    console.log('Fetched user stats:', response.data); // Додаємо логування
-                }
-            } catch (error) {
-                console.error('Error fetching user stats:', error);
-            }
-        };
-
-        fetchUserStats();
-    }, []);
 
     const updateUserStats = (newStats) => {
         setUserStats(prevStats => ({
