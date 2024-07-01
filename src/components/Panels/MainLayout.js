b62cf69a9602b39a07833b8d5a7fde294bb8a7a1
grucia3 <grucia3@gmail.com>
2024-07-01 23:53:11 +0300

    pvp

--- src/components/Panels/MainLayout.js
+++ src/components/Panels/MainLayout.js
@@ -3,6 +3,7 @@ import { Outlet, useLocation } from 'react-router-dom';
 import TopBar from './TopBar';
 import SecondaryBar from './SecondaryBar';
 import NavigationBar from './NavigationBar';
+import SubNavigation from './SubNavigation';
 import './MainLayout.css';
 
 const MainLayout = () => {
