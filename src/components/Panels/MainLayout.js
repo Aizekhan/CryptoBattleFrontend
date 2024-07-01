f7fb9af9d15dc6b820a2765721a8ea303f72cc3b
grucia3 <grucia3@gmail.com>
2024-07-01 23:32:33 +0300

    pvp

--- src/components/Panels/MainLayout.js
+++ src/components/Panels/MainLayout.js
@@ -3,7 +3,6 @@ import { Outlet, useLocation } from 'react-router-dom';
 import TopBar from './TopBar';
 import SecondaryBar from './SecondaryBar';
 import NavigationBar from './NavigationBar';
-import SubNavigation from './SubNavigation';
 import './MainLayout.css';
 
 const MainLayout = () => {
@@ -14,7 +13,6 @@ const MainLayout = () => {
         <div className="main-layout">
             <TopBar isBattlePage={isBattlePage} />
             {!isBattlePage && <SecondaryBar />}
-            {!isBattlePage && <SubNavigation />} {/* Додано SubNavigation */}
             <div className={`content ${isBattlePage ? 'battle-content' : ''}`}>
                 <Outlet /> {/* Тут відображається вміст дочірніх маршрутів */}
             </div>
