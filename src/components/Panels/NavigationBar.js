758c004a092ea8bcff4d5717bf311851c9b2040b
grucia3 <grucia3@gmail.com>
2024-07-02 03:24:43 +0300

    pvp

--- src/components/Panels/NavigationBar.js
+++ src/components/Panels/NavigationBar.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavigationBar.css';
-import homeIcon from '../../assets/images/home-icon.png';
-import friendsIcon from '../../assets/images/friends-icon.png';
-import pickaxeIcon from '../../assets/images/pickaxe-icon.png';
+import farmIcon from '../../assets/images/farm-icon.png';
+import minesIcon from '../../assets/images/mines-icon.png';
 import battleIcon from '../../assets/images/battle-icon.png';
-import shopIcon from '../../assets/images/shop-icon.png';
 import heroIcon from '../../assets/images/hero-icon.png';
+import questsIcon from '../../assets/images/quests-icon.png';
 
 const NavigationBar = ({ isBattlePage }) => {
     return (
         <div className={`navigation-bar ${isBattlePage ? 'hidden-navigation-bar' : ''}`}>
-            <Link to="/home" className="nav-button">
-                <img src={homeIcon} alt="Home" className="icon" />
+            <Link to="/farm" className="nav-button">
+                <img src={farmIcon} alt="Farm" className="icon" />
             </Link>
             <Link to="/mines" className="nav-button">
-                <img src={pickaxeIcon} alt="Mines" className="icon" />
+                <img src={minesIcon} alt="Mines" className="icon" />
             </Link>
             <Link to="/battle" className="nav-button">
                 <img src={battleIcon} alt="Battle" className="icon" />
             </Link>
-            <Link to="/shop" className="nav-button">
-                <img src={shopIcon} alt="Shop" className="icon" />
-            </Link>
             <Link to="/hero" className="nav-button">
                 <img src={heroIcon} alt="Hero" className="icon" />
             </Link>
-            <Link to="/friends" className="nav-button">
-                <img src={friendsIcon} alt="Friends" className="icon" />
+            <Link to="/quests" className="nav-button">
+                <img src={questsIcon} alt="Quests" className="icon" />
             </Link>
         </div>
     );
