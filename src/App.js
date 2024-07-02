a0a1182dde0120cd68204ce7be47c3d625f47e50
grucia3 <grucia3@gmail.com>
2024-07-03 00:45:14 +0300

    pvp

--- src/App.js
+++ src/App.js
@@ -43,42 +43,42 @@ function App() {
     const { updateUserStats } = useUserStats();
 
     useEffect(() => {
-    //    const urlParams = new URLSearchParams(window.location.search);
-     //   const token = urlParams.get('token') || localStorage.getItem('authToken');
-      //  if (token) {
-        //    const decoded = jwt_decode(token);
-         //   updateUserStats({
-           //     username: decoded.username
-       //     });
-//
-     //       localStorage.setItem('authToken', token);
-//
-       //     axios
-          //      .get(`${process.env.REACT_APP_BACKEND_URL}/api/user/${decoded.id}`, {
-          //          headers: {
-           //             Authorization: `Bearer ${token}`,
-             //           "Content-Type": 'application/json',
-             //           "Access-Control-Allow-Origin": "*"
-       //             },
-        //        })
-         //       .then((response) => {
-           //         updateUserStats({
-             //           username: response.data.username,
-              //          level: response.data.level,
-               //         tapIncome: response.data.tapIncome,
-               //         hourlyIncome: response.data.hourlyIncome,
-                 //       balance: response.data.balance,
-                 //       mines: response.data.mines,
-            //            heroes: response.data.heroes,
-         //               currentHeroId: response.data.currentHeroId
-        //            });
-        //        })
-        //        .catch((error) => {
-        //            console.error('Error fetching user data:', error);
-    //            });
-   //     } else {
-     //       console.error('Decoded token does not contain user ID');
-     //   }
+        // const urlParams = new URLSearchParams(window.location.search);
+        // const token = urlParams.get('token') || localStorage.getItem('authToken');
+        // if (token) {
+        //     const decoded = jwt_decode(token);
+        //     updateUserStats({
+        //         username: decoded.username
+        //     });
+        //
+        //     localStorage.setItem('authToken', token);
+        //
+        //     axios
+        //         .get(`${process.env.REACT_APP_BACKEND_URL}/api/user/${decoded.id}`, {
+        //             headers: {
+        //                 Authorization: `Bearer ${token}`,
+        //                 "Content-Type": 'application/json',
+        //                 "Access-Control-Allow-Origin": "*"
+        //             },
+        //         })
+        //         .then((response) => {
+        //             updateUserStats({
+        //                 username: response.data.username,
+        //                 level: response.data.level,
+        //                 tapIncome: response.data.tapIncome,
+        //                 hourlyIncome: response.data.hourlyIncome,
+        //                 balance: response.data.balance,
+        //                 mines: response.data.mines,
+        //                 heroes: response.data.heroes,
+        //                 currentHeroId: response.data.currentHeroId
+        //             });
+        //         })
+        //         .catch((error) => {
+        //             console.error('Error fetching user data:', error);
+        //         });
+        // } else {
+        //     console.error('Decoded token does not contain user ID');
+        // }
     }, [updateUserStats]);
 
     return (
@@ -130,14 +130,6 @@ function App() {
                 </Route>
             </Routes>
         </Router>
-    );
-}
-
-export default function AppWrapper() {
-    return (
-        <UserStatsProvider>
-            <App />
-        </UserStatsProvider>
     );
 }
 
