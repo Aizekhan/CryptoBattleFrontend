a97cf7ac7d4f5821f5750cbe50e7c8fac23750d3
grucia3 <grucia3@gmail.com>
2024-07-03 22:10:41 +0300

    new panel:nav

--- src/App.js
+++ src/App.js
@@ -7,39 +7,7 @@ import './App.css';
 import { useUserStats, UserStatsProvider } from './context/UserStatsContext';
 import Login from './components/Login';
 
-// Імпорт сторінок і субвкладок
-import Home from './components/Pages/Home/Home';
-import News from './components/Pages/Home/News';
-import Market from './components/Pages/Home/Market';
-import Info from './components/Pages/Home/Info';
 
-import Farm from './components/Pages/Farm/Farm';
-import Hunt from './components/Pages/Farm/Hunt';
-import Locations from './components/Pages/Farm/Locations';
-import HeroFarmSkills from './components/Pages/Farm/HeroFarmSkills';
-
-import Mines from './components/Pages/Mines/Mines';
-import MinesGold from './components/Pages/Mines/MinesGold';
-import MiningSkills from './components/Pages/Mines/MiningSkills';
-
-import Battle from './components/Pages/Battle/Battle';
-import HeroDisplay from './components/Pages/Battle/HeroDisplay';
-import PvPBattle from './components/Pages/Battle/BattleScene/PvPBattle';
-import HeroBattleCards from './components/Pages/Battle/HeroBattleCards';
-import Rank from './components/Pages/Battle/Rank';
-
-import Hero from './components/Pages/Hero/Hero';
-import HeroDetails from './components/Pages/Hero/HeroDetails';
-import HeroPassiveSkills from './components/Pages/Hero/HeroPassiveSkills';
-import HeroEquipment from './components/Pages/Hero/HeroEquipment';
-
-import Friends from './components/Pages/Friends/Friends';
-import AllFriends from './components/Pages/Friends/AllFriends';
-
-import Quests from './components/Pages/Quests/Quests';
-import Active from './components/Pages/Quests/Active';
-import Daily from './components/Pages/Quests/Daily';
-import Profa from './components/Pages/Quests/Profa';
 
 function App() {
     const { updateUserStats } = useUserStats();
@@ -83,53 +51,11 @@ function App() {
      //   }
     }, [updateUserStats]);
 
-    return (
+   return (
         <Router>
             <Routes>
                 <Route path="/login" element={<Login />} />
-                <Route path="/" element={localStorage.getItem('authToken') ? <MainLayout /> : <Login />}>
-                    <Route index element={<Home />} />
-                    <Route path="home" element={<Home />}>
-                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to News by default */}
-                        <Route path="sub1" element={<News />} />
-                        <Route path="sub2" element={<Market />} />
-                        <Route path="sub3" element={<Info />} />
-                    </Route>
-                    <Route path="farm" element={<Farm />}>
-                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to Hunt by default */}
-                        <Route path="sub1" element={<Hunt />} />
-                        <Route path="sub2" element={<Locations />} />
-                        <Route path="sub3" element={<HeroFarmSkills />} />
-                    </Route>
-                    <Route path="mines" element={<Mines />}>
-                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to MinesGold by default */}
-                        <Route path="sub1" element={<MinesGold />} />
-                        <Route path="sub2" element={<MiningSkills />} />
-                    </Route>
-                    <Route path="battle" element={<Battle />}>
-                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to HeroDisplay by default */}
-                        <Route path="sub1" element={<HeroDisplay />} />
-                        <Route path="pvp-battle" element={<PvPBattle />} />
-                        <Route path="sub2" element={<HeroBattleCards />} />
-                        <Route path="sub3" element={<Rank />} />
-                    </Route>
-                    <Route path="hero" element={<Hero />}>
-                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to HeroDetails by default */}
-                        <Route path="sub1" element={<HeroDetails />} />
-                        <Route path="sub2" element={<HeroPassiveSkills />} />
-                        <Route path="sub3" element={<HeroEquipment />} />
-                    </Route>
-                    <Route path="friends" element={<Friends />}>
-                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to AllFriends by default */}
-                        <Route path="sub1" element={<AllFriends />} />
-                    </Route>
-                    <Route path="quests" element={<Quests />}>
-                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to Active by default */}
-                        <Route path="sub1" element={<Active />} />
-                        <Route path="sub2" element={<Daily />} />
-                        <Route path="sub3" element={<Profa />} />
-                    </Route>
-                </Route>
+                <Route path="*" element={localStorage.getItem('authToken') ? <MainLayout /> : <Login />} />
             </Routes>
         </Router>
     );
--- src/components/Panels/MainLayout.js
+++ src/components/Panels/MainLayout.js
@@ -1,22 +1,102 @@
 import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import TopBar from './TopBar';
-import SecondaryBar from './SecondaryBar';
-import NavigationBar from './NavigationBar';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import './MainLayout.css';
+import battleIcon from '../../assets/icons/NavPanel/battle-icon.png';
+import farmIcon from '../../assets/icons/NavPanel/farm-icon.png';
+import friendsIcon from '../../assets/icons/NavPanel/friends-icon.png';
+import heroIcon from '../../assets/icons/NavPanel/hero-icon.png';
+import homeIcon from '../../assets/icons/NavPanel/home-icon.png';
+import minesIcon from '../../assets/icons/NavPanel/mines-icon.png';
+import questsIcon from '../../assets/icons/NavPanel/quests-icon.png';
+import Window from '../../assets/icons/NavPanel/Window.png';
+
+import Home from '../Pages/Home/Home';
+import Farm from '../Pages/Farm/Farm';
+import Battle from '../Pages/Battle/Battle';
+import Hero from '../Pages/Hero/Hero';
+import Quests from '../Pages/Quests/Quests';
+import Friends from '../Pages/Friends/Friends';
+import MinesGold from '../Pages/Mines/MinesGold';
+import MiningSkills from '../Pages/Mines/MiningSkills';
+import News from '../Pages/Home/News';
+import Market from '../Pages/Home/Market';
+import Info from '../Pages/Home/Info';
+import Hunt from '../Pages/Farm/Hunt';
+import Locations from '../Pages/Farm/Locations';
+import HeroFarmSkills from '../Pages/Farm/HeroFarmSkills';
+import HeroDisplay from '../Pages/Battle/HeroDisplay';
+import PvPBattle from '../Pages/Battle/PvPBattle';
+import HeroBattleCards from '../Pages/Battle/HeroBattleCards';
+import Rank from '../Pages/Battle/Rank';
+import HeroDetails from '../Pages/Hero/HeroDetails';
+import HeroPassiveSkills from '../Pages/Hero/HeroPassiveSkills';
+import HeroEquipment from '../Pages/Hero/HeroEquipment';
+import AllFriends from '../Pages/Friends/AllFriends';
+import Active from '../Pages/Quests/Active';
+import Daily from '../Pages/Quests/Daily';
+import Profa from '../Pages/Quests/Profa';
 
 const MainLayout = () => {
-    const location = useLocation();
-    const isBattlePage = location.pathname.startsWith('/battle/pvp-battle');
-
     return (
         <div className="main-layout">
-            <TopBar isBattlePage={isBattlePage} />
-            {!isBattlePage && <SecondaryBar />}
-            <div className={`content ${isBattlePage ? 'battle-content' : ''}`}>
+            <div className="top-bar">
+                <img src={battleIcon} alt="Battle" className="icon" />
+                <img src={Window} alt="Window" className="window-icon" />
+                <img src={farmIcon} alt="Farm" className="icon" />
+                <img src={friendsIcon} alt="Friends" className="icon" />
+            </div>
+            <div className="content">
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="home" element={<Home />}>
+                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to News by default */}
+                        <Route path="sub1" element={<News />} />
+                        <Route path="sub2" element={<Market />} />
+                        <Route path="sub3" element={<Info />} />
+                    </Route>
+                    <Route path="farm" element={<Farm />}>
+                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to Hunt by default */}
+                        <Route path="sub1" element={<Hunt />} />
+                        <Route path="sub2" element={<Locations />} />
+                        <Route path="sub3" element={<HeroFarmSkills />} />
+                    </Route>
+                    <Route path="mines" element={<MinesGold />}>
+                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to MinesGold by default */}
+                        <Route path="sub1" element={<MinesGold />} />
+                        <Route path="sub2" element={<MiningSkills />} />
+                    </Route>
+                    <Route path="battle" element={<Battle />}>
+                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to HeroDisplay by default */}
+                        <Route path="sub1" element={<HeroDisplay />} />
+                        <Route path="pvp-battle" element={<PvPBattle />} />
+                        <Route path="sub2" element={<HeroBattleCards />} />
+                        <Route path="sub3" element={<Rank />} />
+                    </Route>
+                    <Route path="hero" element={<Hero />}>
+                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to HeroDetails by default */}
+                        <Route path="sub1" element={<HeroDetails />} />
+                        <Route path="sub2" element={<HeroPassiveSkills />} />
+                        <Route path="sub3" element={<HeroEquipment />} />
+                    </Route>
+                    <Route path="friends" element={<Friends />}>
+                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to AllFriends by default */}
+                        <Route path="sub1" element={<AllFriends />} />
+                    </Route>
+                    <Route path="quests" element={<Quests />}>
+                        <Route index element={<Navigate to="sub1" />} /> {/* Redirect to Active by default */}
+                        <Route path="sub1" element={<Active />} />
+                        <Route path="sub2" element={<Daily />} />
+                        <Route path="sub3" element={<Profa />} />
+                    </Route>
+                </Routes>
                 <Outlet /> {/* Тут відображається вміст дочірніх маршрутів */}
             </div>
-            {!isBattlePage && <NavigationBar isBattlePage={isBattlePage} />}
+            <div className="bottom-bar">
+                <img src={homeIcon} alt="Home" className="icon" />
+                <img src={heroIcon} alt="Hero" className="icon" />
+                <img src={minesIcon} alt="Mines" className="icon" />
+                <img src={questsIcon} alt="Quests" className="icon" />
+            </div>
         </div>
     );
 };
