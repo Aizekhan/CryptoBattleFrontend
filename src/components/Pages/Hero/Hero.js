47202e905662b598aca6e602e13edbe4f13dd94c
grucia3 <grucia3@gmail.com>
2024-07-05 16:50:45 +0300

    hero

--- src/components/Pages/Hero/Hero.js
+++ src/components/Pages/Hero/Hero.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import HeroStatsCard from './HeroDetails/HeroStatsCard'; // Імпортуємо компонент HeroStatsCard
-import { useUserStats } from '../../../context/UserStatsContext'; // Імпортуємо контекст для отримання статистики героя
 
 const Hero = () => {
-    const { userStats } = useUserStats(); // Отримуємо дані користувача з контексту
-    const currentHero = userStats.heroes.find(hero => hero.id === userStats.currentHeroId) || {}; // Знаходимо поточного героя
-
     return (
         <div className="hero-page">
             <Outlet /> {/* Тут відображається вміст дочірніх маршрутів */}
-            <div className="hero-stats-container">
-                <HeroStatsCard stats={currentHero.baseStats} /> {/* Відображення картки HeroStatsCard */}
-            </div>
         </div>
     );
 };
