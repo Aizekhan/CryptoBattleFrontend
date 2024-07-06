637685e22e88ba4c7467b46ac352d734a480a944
grucia3 <grucia3@gmail.com>
2024-07-06 18:14:12 +0300

    pvp

--- src/components/Cards/CardList.js
+++ src/components/Cards/CardList.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Card from './Card'; // Імпорт компоненту Card
+import Card from './Card';
 import './CardList.css';
 
 const CardList = ({ cards }) => {
     return (
         <div className="card-list">
-            {cards.map(card => (
-                <div className="card-container" key={card.id}>
-                    <Card card={card} />
+            {cards.map((card, index) => (
+                <div key={index} className="card-container">
+                    <Card {...card} />
                 </div>
             ))}
         </div>
