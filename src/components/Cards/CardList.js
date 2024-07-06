abfc2f5ba82f13c0e717993d228b843f422ba1a4
Aizekhan <hrytsenkomaksym@gmail.com>
2024-07-06 14:18:45 +0300

    card

--- src/components/Cards/CardList.js
+++ src/components/Cards/CardList.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Card from './Card';
+import Card from './Card'; // Імпорт компоненту Card
 import './CardList.css';
 
 const CardList = ({ cards }) => {
     return (
         <div className="card-list">
             {cards.map(card => (
-                <Card key={card.id} card={card} />
+                <div className="card-container" key={card.id}>
+                    <Card card={card} />
+                </div>
             ))}
         </div>
     );
