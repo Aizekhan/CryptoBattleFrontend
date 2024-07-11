18a33c0e877b285abd1fd5048911948411996f20
Aizekhan <hrytsenkomaksym@gmail.com>
2024-07-11 13:37:05 +0300

    cards

--- src/components/Cards/UpgradeButton.js
+++ src/components/Cards/UpgradeButton.js
@@ -22,25 +22,24 @@ const UpgradeButton = ({ card, onClose }) => {
                 case 'goldMine':
                     const currentLevel = userStats.mines.find(c => c.id === card.id).level;
                     newHourlyIncome += card.baseIncome * Math.pow(card.scaleIncome, currentLevel + 1);
+                    const updatedMines = userStats.mines.map(c =>
+                        c.id === card.id ? { ...c, level: c.level + 1, upgradeCost: newUpgradeCost } : c
+                    );
+                    updateUserStats({
+                        mines: updatedMines
+                    });
                     break;
 
                 case 'heroStat':
-                    newHeroStats[card.effectType] += card.effectValue;
-                    updateHeroStats(userStats.currentHeroId, newHeroStats);
-                    break;
-
                 case 'equip':
-                    // Логіка для апгрейду спорядження
-                    break;
-
                 case 'battleCard':
-                    // Логіка для апгрейду бойових карток
-                    break;
-
                 case 'farmSkill':
-                    if (card.effectType === 'regenSpeed') {
-                        newHeroStats.regenSpeed = (newHeroStats.regenSpeed || 0) + card.effectValue;
-                    }
+                    const cardType = card.tag;
+                    const updatedCards = newHeroStats[cardType].map(c =>
+                        c.id === card.id ? { ...c, level: c.level + 1, upgradeCost: newUpgradeCost } : c
+                    );
+                    newHeroStats[cardType] = updatedCards;
+                    updateHeroStats(userStats.currentHeroId, newHeroStats);
                     break;
 
                 default:
@@ -49,19 +48,9 @@ const UpgradeButton = ({ card, onClose }) => {
 
             newUpgradeCost = Math.floor(card.upgradeCost * card.scaleUpgrade);
 
-            const updatedMines = userStats.mines.map(c =>
-                c.id === card.id ? { ...c, level: c.level + 1, upgradeCost: newUpgradeCost } : c
-            );
-
-            const updatedHeroes = userStats.heroes.map(hero =>
-                hero.id === userStats.currentHeroId ? newHeroStats : hero
-            );
-
             updateUserStats({
                 balance: userStats.balance - card.upgradeCost,
-                hourlyIncome: newHourlyIncome,
-                mines: updatedMines,
-                heroes: updatedHeroes
+                hourlyIncome: newHourlyIncome
             });
 
             onClose(); // Закриваємо модальне вікно після апгрейду
