db1ed250b98f425243ea0bac2fa566fe55a9b135
grucia3 <grucia3@gmail.com>
2024-07-11 21:05:14 +0300

    sad

--- src/components/Pages/Mines/MinesGold.js
+++ src/components/Pages/Mines/MinesGold.js
@@ -2,24 +2,21 @@ import React from 'react';
 import CardList from '../../Cards/CardList';
 import { useUserStats } from '../../../context/UserStatsContext';
 
-const HeroFarmSkills = () => {
+const MinesGold = () => {
     const { userStats } = useUserStats();
     const currentHeroId = userStats.currentHeroId;
 
     // Отримуємо всі карти для поточного героя
     const heroCards = userStats.heroes.find(hero => hero.id === currentHeroId)?.cards || [];
 
-    // Отримуємо фермерські навички для поточного героя
-    const heroFarmSkills = heroCards.filter(card => card.tag === 'farmSkill');
-
-    // Отримуємо першу фермерську навичку
-    const firstHeroFarmSkill = heroFarmSkills.length > 0 ? [heroFarmSkills[0]] : [];
+    // Отримуємо золоті шахти для поточного героя
+    const heroGoldMines = heroCards.filter(card => card.tag === 'goldMine');
 
     return (
-        <div className="hero-farm-skills">
-            <CardList cards={firstHeroFarmSkill} />
+        <div className="mines-gold">
+            <CardList cards={heroGoldMines} />
         </div>
     );
 };
 
-export default HeroFarmSkills;
+export default MinesGold;
